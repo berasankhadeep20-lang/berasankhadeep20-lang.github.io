571faf1e3b434bded365ffe929c09efda23da39e
berasankhadeep20-lang <berasankhadeep20@gamil.com>
2026-06-26 19:55:54 +0530

    Fix

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: mode === "production" ? "/website-glow-up/" : "/",
+  base: "/",
   server: {
     host: "::",
     port: 8080,
